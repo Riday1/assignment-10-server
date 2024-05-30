ffb26e317129183bf9d60b031220dc67fff05390
Riday1 <rrriday100@gmail.com>
2024-05-30 23:53:46 +0600

    courses routes created, this route will help to fetch all data of courses

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ app.get('/categories/:id', (req, res) => {
     res.send(categoryData)
 })
 
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+
 
 app.listen(port, () => {
     console.log('example app listening')
